bfefc0f0072e9f5dd01d7b1842a5528f814c9dea
EVolgina <123949683+EVolgina@users.noreply.github.com>
2024-07-28 14:50:59 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Использование существующего сервисного аккаунта
 locals {
-  service_account_id = "ajetnpf6s7pt87g5bdvt"
+  service_account_id = var.service_account_id
 }
 # Create Static Access Keys
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
@@ -22,25 +22,28 @@ resource "yandex_vpc_network" "netology-net" {
 }
 
 # Создание подсети
+
 resource "yandex_vpc_subnet" "subnet-a" {
-  name           = "subnet-a"
+  name = "subnet-a"
+  v4_cidr_blocks = ["10.5.0.0/16"]
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.netology-net.id
-  v4_cidr_blocks = ["10.0.0.0/24"]
 }
 
 resource "yandex_vpc_subnet" "subnet-b" {
-  name           = "subnet-b"
+  name = "subnet-b"
+  v4_cidr_blocks = ["10.6.0.0/16"]
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.netology-net.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
 }
-#resource "yandex_vpc_subnet" "subnet-d" {
-#  name           = "subnet-d"
-#  zone           = "ru-central1-d"
-#  network_id     = yandex_vpc_network.netology-net.id
-#  v4_cidr_blocks = ["10.0.2.0/24"]
-#}
+
+resource "yandex_vpc_subnet" "subnet-d" {
+  name = "subnet-d"
+  v4_cidr_blocks = ["10.7.0.0/16"]
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.netology-net.id
+}
+
 # Конфигурация Terraform
 terraform {
   backend "s3" {
@@ -49,96 +52,152 @@ terraform {
     region = "ru-central1"
     key        = "terraform/state"
     access_key = "YCAJEycW1ufgWEU9TD-xTdzqp"
-    secret_key = "YCPrR_XbGDC7JF5h2KY0TPM4-3P5YPKYz8PIfhqq"
+      secret_key = "YCPrR_XbGDC7JF5h2KY0TPM4-3P5YPKYz8PIfhqq"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
+
 # Создание группы целей для балансировщика нагрузки
 resource "yandex_lb_target_group" "target-group" {
   name = "target-group"
   target {
     subnet_id = yandex_vpc_subnet.subnet-a.id
-    address   = "10.0.0.10"
+    address   = "10.5.0.10"
   }
   target {
     subnet_id = yandex_vpc_subnet.subnet-b.id
-    address   = "10.0.1.10"
+    address   = "10.6.0.10"
   }
-#  target {
-#    subnet_id = yandex_vpc_subnet.subnet-d.id
-#    address   = "10.0.2.10"
-#  }
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-d.id
+    address   = "10.7.0.10"
+  }
+  timeouts {
+    create = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+}
+##
+resource "yandex_lb_target_group" "ssh_target_group" {
+  name      = "ssh-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-a.id
+    address   = "10.5.0.11"
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-b.id
+    address   = "10.6.0.11"
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-d.id
+    address   = "10.7.0.11"
+  }
 }
 # Создание группы виртуальных машин
 resource "yandex_compute_instance_group" "lamp-group" {
   name               = "lamp-group"
   folder_id          = "b1gpoeqn2q7if0pboa4u"
-  service_account_id = local.service_account_id
+  service_account_id = var.service_account_id
   instance_template {
-    platform_id = "standard-v1"
-    resources {
+    platform_id = "standard-v2"
+        resources {
       cores  = 2
       memory = 2
-      core_fraction = 20
+      core_fraction = 5
       }
     boot_disk {
       initialize_params {
         image_id = "fd827b91d99psvq5fjit"
       }
     }
+    metadata = {
+      user-data = file("/home/vagrant/diplom/cloud.yaml")
+    }
+
     scheduling_policy {
          preemptible = true
       }
-    network_interface {
-      subnet_ids = [yandex_vpc_subnet.subnet-a.id, yandex_vpc_subnet.subnet-b.id]
+     network_interface {
+       nat = true
+       subnet_ids = [
+        yandex_vpc_subnet.subnet-a.id,
+        yandex_vpc_subnet.subnet-b.id,
+        yandex_vpc_subnet.subnet-d.id
+      ]
     }
-  }
-  scale_policy {
-    fixed_scale {
-      size = 2
+ }
+     scale_policy {
+      fixed_scale {
+        size = 3
     }
   }
   allocation_policy {
-    zones = ["ru-central1-a", "ru-central1-b"]
+    zones = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
   }
   deploy_policy {
     max_unavailable = 1
     max_expansion = 2
   }
   health_check {
-#    initial_delay = 10
-#    interval      = 5
-#    timeout       = 4
-#    healthy_threshold   = 2
-#    unhealthy_threshold = 2
+    interval      = 10
+    timeout       = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
     tcp_options {
       port = 80
     }
   }
 }
-# Создание сетевого балансировщика
-resource "yandex_lb_network_load_balancer" "vp-nlb-1" {
-  name = "network-load-balancer-1"
 
-  listener {
-     name = "network-load-balancer-1-listener"
+# Создание сетевого балансировщика
+resource "yandex_lb_network_load_balancer" "my-load-balancer" {
+  name = "my-load-balancer"
+ listener {
+     name = "http-listener"
      port = 80
      external_address_spec {
      ip_version = "ipv4"
     }
  }
+  listener {
+    name = "ssh-listener"
+    port = 22
+    target_port = 22
+    external_address_spec {
+    ip_version = "ipv4"
+    }
+ }
   attached_target_group {
     target_group_id = yandex_lb_target_group.target-group.id
+
     healthcheck {
       name = "http"
-      interval = 2
-      timeout = 1
+      interval = 10
+      timeout = 5
       unhealthy_threshold = 2
       healthy_threshold = 5
       http_options {
         port = 80
         path = "/"
+      }
+    }
+ }
+ attached_target_group {
+   target_group_id = yandex_lb_target_group.ssh_target_group.id
+    healthcheck {
+      name               = "ssh"
+      interval           = 10
+      timeout            = 5
+      unhealthy_threshold = 2
+      healthy_threshold  = 2
+      tcp_options {
+        port = 22
       }
     }
   }
